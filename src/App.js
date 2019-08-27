caae1db98328aa781acb2f0b8d9c9956d7f47c70
Kayla Anderson <kaylanicolette@live.com>
2019-08-27 23:44:43 +0000

    Setting up initial project structure

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Grid style={{ justifyContent: 'center' }} container spacing={10}>
+        <Grid item xs={11}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              // {/* This is where SearchBar component will go */}
+            </Grid>
+            <Grid item xs={8}>
+              // {/* This is where VideoDetail component will go */}
+            </Grid>
+            <Grid item xs={4}>
+              // {/* This is where VideoList component will go */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default App;
